7309ff6d11cbfcf2d05d4c01ccca9c3a8d671486
root <root@joyce-hp>
2020-08-21 21:47:32 +0200

    Plotting the cost function

--- BatchGradientDescent.py
+++ BatchGradientDescent.py
@@ -5,7 +5,7 @@ Created on Thu Aug 20 19:08:00 2020
 
 @author: root
 """
-
+import matplotlib.pyplot as plt 
 #  functions to use in Gradient Descent method
 def hypothesis(theta, x):
         
@@ -22,12 +22,20 @@ def cost(theta,m,X,y):
 def gradientDescent(initial_theta,alpha,X,y,m, iterations):
     i = 1
     theta = initial_theta
+    J = []
     while i <= iterations: 
         
         h = hypothesis(theta, X)
 
         term_update = ((h-y)*X).sum(axis=0)      
         theta = theta - (alpha *term_update/m) 
+        J.append(cost(theta,m,X,y))
         i = i + 1 
+        
+    plt.plot(J,label = 'cost')
+    plt.ylabel('cost', fontsize = 14)
+    plt.xlabel('iteration', fontsize = 14)
+    plt.show()
+    print(J[-1])
         
     return theta
